31f4c1d88d0e1d9bf7ba486b7ab0a1328ac949d6
Lawrence Lim <ll4715@nyu.edu>
2024-03-06 15:08:53 -0500

    feat: added collect vars function with slight modification from prev implementation

--- compile.ml
+++ compile.ml
@@ -1,5 +1,6 @@
 (* Compile Cish AST to MIPS AST *)
 open Mips
+open Ast
 
 exception IMPLEMENT_ME
 
@@ -13,6 +14,76 @@ let new_int () =
   !label_counter
 
 let new_label () = "L" ^ string_of_int (new_int ())
+
+(* sets of variables -- Ocaml Set and Set.S *)
+module VarSet = Set.Make (struct
+  type t = Ast.var
+
+  let compare = String.compare
+end)
+
+(* a table of variables that we need for the code segment *)
+let variables : VarSet.t ref = ref VarSet.empty
+
+(* generate a fresh temporary variable and store it in the variables set. *)
+(*NOTE: Likely used in collect vars*)
+let rec new_temp () =
+  let t = "T" ^ string_of_int (new_int ()) in
+  (* make sure we don't already have a variable with the same name! *)
+  if VarSet.mem t !variables then new_temp ()
+  else (
+    variables := VarSet.add t !variables;
+    t)
+
+(* reset internal state *)
+let reset () =
+  label_counter := 0;
+  variables := VarSet.empty
+
+(* find all of the variables in a program and add them to
+ * the set variables *)
+(* NOTE: This return unit (which is like None) *)
+let rec collect_vars_exp ((e, _) : exp) =
+  match e with
+  | Int _ -> ()
+  | Var x -> variables := VarSet.add x !variables
+  | Binop (e1, _, e2) ->
+      collect_vars_exp e1;
+      collect_vars_exp e2
+  | Not e -> collect_vars_exp e
+  | And (e1, e2) | Or (e1, e2) ->
+      collect_vars_exp e1;
+      collect_vars_exp e2
+  | Assign (x, e) ->
+      variables := VarSet.add x !variables;
+      collect_vars_exp e
+  | _ -> raise IMPLEMENT_ME
+
+let rec collect_vars_stmt ((s, _) : stmt) =
+  match s with
+  | Exp e -> collect_vars_exp e
+  | Seq (s1, s2) ->
+      collect_vars_stmt s1;
+      collect_vars_stmt s2
+  | If (e, s1, s2) ->
+      collect_vars_exp e;
+      collect_vars_stmt s1;
+      collect_vars_stmt s2
+  | While (e, s) ->
+      collect_vars_exp e;
+      collect_vars_stmt s
+  | For (e1, e2, e3, s) ->
+      collect_vars_exp e1;
+      collect_vars_exp e2;
+      collect_vars_exp e3;
+      collect_vars_stmt s
+  | Return e -> collect_vars_exp e
+  | _ -> raise IMPLEMENT_ME
+
+(* FIXME: This expression has type program = func list but an expression was expected of type stmt = rstmt * pos*)
+let rec collect_vars (p : Ast.program) : unit =
+  (*************************************************************)
+  collect_vars_stmt p
 
 (*TODO: Implement compile function*)
 let rec compile (p : Ast.program) : result = raise IMPLEMENT_ME
