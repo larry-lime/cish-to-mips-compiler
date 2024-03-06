cfca2b39805398e5df7e280a239f4ae4f6a96f47
Lawrence Lim <ll4715@nyu.edu>
2024-03-05 20:57:09 -0500

    chore: formatted code

--- cish.ml
+++ cish.ml
@@ -6,17 +6,17 @@ open Eval
  * You do not need to understand this interaction with the system. *)
 let parse_file () =
   let argv = Sys.argv in
-  let _ = 
-    if Array.length argv != 2
-    then (prerr_string ("usage: " ^ argv.(0) ^ " [file-to-compile]\n");
-    exit 1) in
+  let _ =
+    if Array.length argv != 2 then (
+      prerr_string ("usage: " ^ argv.(0) ^ " [file-to-compile]\n");
+      exit 1)
+  in
   let ch = open_in argv.(1) in
   Parse.program Lex.lexer (Lexing.from_channel ch)
 
-let compile_prog prog =
-  Compile.result2string (Compile.compile prog )
+let compile_prog prog = Compile.result2string (Compile.compile prog)
 
 (* Usage ps4 [file-to-compile] *)
 let _ =
-  let prog = parse_file() in
-  print_string (compile_prog prog) 
+  let prog = parse_file () in
+  print_string (compile_prog prog)
--- word32.ml
+++ word32.ml
@@ -11,12 +11,9 @@ let fromInt n = Int32.of_int n
 (* You can ignore the functions below. They are just used in the Mips simulator *)
 let toString w = Int32.to_string w
 
-let toStringX w = 
-  if w = 0x80000000l
-  then "0x80000000"
-  else Printf.sprintf "0x%lX" w
+let toStringX w =
+  if w = 0x80000000l then "0x80000000" else Printf.sprintf "0x%lX" w
 
-let andb (w1,w2) = Int32.logand w1 w2
-
+let andb (w1, w2) = Int32.logand w1 w2
 let neg w = Int32.neg w
 
